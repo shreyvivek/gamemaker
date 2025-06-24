37e5744a7d1741820ae3c0b834ba48551950201e
shreyvivek <shreyv002@gmail.com>
2025-06-25 01:41:16 +0800

    trying prop.lean alone in game.lean

--- Game.lean
+++ Game.lean
@@ -1,22 +1,8 @@
 
 import GameServer.Commands
 
---import Game.Levels.Propositional.Propositional
-import Game.Levels.Propositional.L01_AssumeAndFinish
-import Game.Levels.Propositional.L02_IntroduceAssumption
-import Game.Levels.Propositional.L03_AndIntroduction
-import Game.Levels.Propositional.L04_AndEliminationLeft
-import Game.Levels.Propositional.L05_OrIntroductionLeft
-import Game.Levels.Propositional.L06_OrIntroductionRight
-import Game.Levels.Propositional.L07_OrElimination
-import Game.Levels.Propositional.L08_ImplicationChain
-import Game.Levels.Propositional.L09_ModusPonens
-import Game.Levels.Propositional.L10_NegationIntroduction
-import Game.Levels.Propositional.L11_ModusTollens
-import Game.Levels.Propositional.L12_Contrapositive
-import Game.Levels.Propositional.L13_DisjunctiveSyllogism
-import Game.Levels.Propositional.L14_DoubleNegation
-import Game.Levels.Propositional.L15_Dilemma
+import Game.Levels.Propositional.Propositional
+
 
 -- Here's what we'll put on the title screen
 Title "Discrete Mathematics with Lean4"
