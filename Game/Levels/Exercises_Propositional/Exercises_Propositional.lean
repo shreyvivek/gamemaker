c79498a574b53b8705527db38dfd9fc77b687877
shreyvivek <shreyv002@gmail.com>
2025-07-02 05:09:27 +0800

    new levelssss

---
+++ Game/Levels/Exercises_Propositional/Exercises_Propositional.lean
@@ -0,0 +1,16 @@
+import GameServer.Commands
+import Game.Levels.Propositional.Propositional
+import Game.Levels.Exercises_Propositional.L01_PracticeProp
+World "Exercises_Propositional"
+
+Title "Exercises - Propositional"
+
+Introduction "
+In this world, you'll try out some exercises on propositional logic, based on the knowledge gained so far.
+
+Before you start, take note of the panel on the right (if you are on PC). You can find the documentations/descriptions for several **Tactics** we will be making use of, and **Theorems** we will be proving(_and using_) in this World. Click on them, whether locked or unlocked, to read and understand! You are encouraged to do so whenever you are prompted to use a tactic in the game.
+
+Click on the `Start` button to begin!
+
+
+"
