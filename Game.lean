013c7f85c0892e172be59cddb5bce18a39d2c49c
shreyvivek <shreyv002@gmail.com>
2025-07-28 03:05:29 +0800

    dependency change

--- Game.lean
+++ Game.lean
@@ -41,7 +41,7 @@ CoverImage "images/MH1812 Logo.png"
 Dependency Propositional → Exercises_Propositional
 Dependency Propositional → Predicate
 Dependency Predicate → Induction
-Dependency Propositional → Sets
+Dependency Predicate → Sets
 
 
 /-! Build the game. Show's warnings if it found a problem with your game. -/
